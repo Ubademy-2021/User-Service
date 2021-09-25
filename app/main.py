1bf8fc67942742deaf90afe82245ead18cf3a5f5
Fernando <ffabbiano@fi.uba.ar>
2021-09-25 20:08:03 -0300

    Cambios para probar deploy en Heroku

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import uuid
 import requests
 import uvicorn
@@ -114,4 +115,4 @@ async def delete_users(user_id: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=5000)
+    uvicorn.run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
