7e26dc3077a25b81d4d187a0935fa62ad7aee49c
Martin <martincohen98@gmail.com>
2021-10-03 23:15:09 -0300

    Adding basic logs for crud

--- app/adapters/http/users/users_controller.py
+++ app/adapters/http/users/users_controller.py
@@ -1,3 +1,4 @@
+import logging
 from app.adapters.database.database import SessionLocal
 from app.domain.users.model.user import UserCreate, User
 from app.domain.users.repository.user_repository import UserRepository
@@ -14,30 +15,38 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception as e:
+        logging.critical("Database connection failed: " + e.__str__)
     finally:
         db.close()
 
 
 @router.post("/users", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    logging.info("Creating user " + user.email)
     crud = UserRepository(db)
     db_user = crud.get_user_by_email(email=user.email)
     if db_user:
+        logging.warn("User " + user.email + " already exsists")
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(user=user)
 
 
 @router.get("/users", response_model=List[User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    logging.info("Getting users list")
     crud = UserRepository(db)
     users = crud.get_users(skip=skip, limit=limit)
+    logging.debug("Getting " + users.count + " users")
     return users
 
 
 @router.get("/users/{user_id}", response_model=User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
+    logging.info("Getting user with id = " + user_id)
     crud = UserRepository(db)
     db_user = crud.get_user(user_id=user_id)
     if db_user is None:
+        logging.warning("User with id = " + user_id + " not found")
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 import uvicorn
+import logging
 from fastapi import FastAPI, status
 from app.adapters.http.users import users_controller
 from app.adapters.database.users.model import Base
@@ -10,9 +11,12 @@ Base.metadata.create_all(bind=engine)
 # Create app with FAST API
 app = FastAPI(debug=True)
 
+logging.info("Starting User-Service")
+
 
 @app.get('/ping', status_code=status.HTTP_200_OK)
 async def root():
+    logging.warn("This is an testing endpoint, not intended for productive environment")
     return "pong"
 
 
@@ -20,4 +24,5 @@ app.include_router(users_controller.router, prefix="/api")
 
 if __name__ == "__main__":
     port = os.environ.get('PORT', 5000)
+    logging.info("Using port: " + port)
     uvicorn.run(app, host='0.0.0.0', port=port)
