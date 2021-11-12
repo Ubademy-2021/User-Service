8b14a1ec25656f2646869b4e45db5f92569121c4
Martin <martincohen98@gmail.com>
2021-11-12 13:02:38 -0300

    Added admin test to api test

--- tests/api_test.py
+++ tests/api_test.py
@@ -7,3 +7,8 @@ client = TestClient(app)
 def test_get_users():
     response = client.get("/api/users")
     assert response.status_code == 200
+
+
+def test_get_admins():
+    response = client.get("/api/admins")
+    assert response.status_code == 200
